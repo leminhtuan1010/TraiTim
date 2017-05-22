f29ed21afb909cd5f2c3a55d74922f391eab7908
leminhtuan1010 <leminhtuan1010@gmail.com>
2017-05-22 17:45:06 +0700

    first commit

--- Traitim/ViewController.swift
+++ Traitim/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var anh: UIImageView!
+    @IBOutlet weak var lbl_text: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.lbl_text.text = "I Love You"
+        addAnh()
+        kichThuoc()
+        
+    }
+    func addAnh(){
+        anh = UIImageView(frame: CGRect(x: 155, y: 200, width: 100, height: 100))
+        anh.image = UIImage(named: "heart")
+        view.addSubview(anh)
+    }
+    func kichThuoc(){
+        UIView.animate(withDuration: 3, animations: {
+            self.anh.transform = CGAffineTransform(scaleX: 3.0, y: 3.0)
+            self.lbl_text.alpha = 0
+        }) { (complete) in
+            UIView.animate(withDuration: 3, animations: {
+                self.anh.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                self.lbl_text.alpha = 1
+            }, completion:{(complete) in
+                self.kichThuoc()
+            })
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
